596d6ce8dd582b4b26bf021e2202b3ae224efcfd
michael@mharris.io <19492392+Burwin@users.noreply.github.com>
2022-01-20 11:57:32 -0500

    find lectionary and reading nodes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,45 @@
-export function getCatholicDailyReadings(date?: Date) {
-  console.log("Hello, Father");
+import puppeteer from "puppeteer";
+import dateformat from "dateformat";
+
+export async function getCatholicDailyReadings(date?: Date) {
+  // get page source
+  date = date ?? new Date();
+  const formattedDate = dateformat(date, "mmddyy");
+  const url = `https://bible.usccb.org/bible/readings/${formattedDate}.cfm`;
+
+  // logger.info("launching browser...");
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--disable-gpu",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--no-first-run",
+      "--no-sandbox",
+      "--no-zygote",
+      "--single-process",
+      "--proxy-server='direct://'",
+      "--proxy-bypass-list=*",
+      "--deterministic-fetch",
+      "--font-render-hinting=medium",
+    ],
+  });
+
+  //   logger.info("starting page...");
+  const page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+  );
+  await page.emulateTimezone("America/Detroit");
+
+  // load page
+  await page.goto(url, { waitUntil: "networkidle0" });
+
+  // find lectionary header by class 'b-lectionary'
+  const lectionaryNode = await page.$(".b-lectionary");
+
+  // find readings by class 'b-verse'
+  const readingNodes = await page.$$(".b-verse");
+
+  return [lectionaryNode, ...readingNodes];
 }
